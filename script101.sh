bdbd356f673f88e8dbc560f6e9692d6f3da2ba78
Mikelersky <jmqueddeng11@gmail.com>
2026-07-30 08:12:10 -0700

    Add files via upload

---
+++ script101.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+mkdir -p OS/P1
+mkdir -p OS/P4
+mkdir -p OS/Network/P2
+mkdir -p OS/Network/P3
+
+mkdir -p Script/P1
+mkdir -p Script/P3
+
+touch OS/P4/ReadMe1.txt
+touch OS/P4/ReadMe2.txt
+
+touch OS/Network/P2/ReadMe1.txt
+touch OS/Network/P3/ReadMe2.txt
+
+touch Script/P1/ReadMe1.txt
+
+echo "Directory structure created successfully!"
